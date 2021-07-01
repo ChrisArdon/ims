7e70851165b7e54f305a06dbaf1b971cd18c68ec
Chris Ardon <rockrisbl@gmail.com>
2021-06-30 19:01:11 -0600

    Fields added in Supplier form

--- ims/Supplier.Designer.cs
+++ ims/Supplier.Designer.cs
@@ -29,6 +29,18 @@ namespace ims
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.supplierCompanyTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.personNameTxt = new System.Windows.Forms.TextBox();
+            this.Phone1Txt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.phone2Txt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.addressTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ntnTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.leftPanel.SuspendLayout();
             this.rightPanel.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -40,7 +52,33 @@ namespace ims
             // 
             // leftPanel
             // 
+            this.leftPanel.Controls.Add(this.ntnTxt);
+            this.leftPanel.Controls.Add(this.label7);
+            this.leftPanel.Controls.Add(this.addressTxt);
+            this.leftPanel.Controls.Add(this.label6);
+            this.leftPanel.Controls.Add(this.phone2Txt);
+            this.leftPanel.Controls.Add(this.label5);
+            this.leftPanel.Controls.Add(this.Phone1Txt);
+            this.leftPanel.Controls.Add(this.label4);
+            this.leftPanel.Controls.Add(this.personNameTxt);
+            this.leftPanel.Controls.Add(this.label3);
+            this.leftPanel.Controls.Add(this.supplierCompanyTxt);
+            this.leftPanel.Controls.Add(this.label2);
             this.leftPanel.Size = new System.Drawing.Size(250, 780);
+            this.leftPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.leftPanel_Paint);
+            this.leftPanel.Controls.SetChildIndex(this.panel1, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label2, 0);
+            this.leftPanel.Controls.SetChildIndex(this.supplierCompanyTxt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label3, 0);
+            this.leftPanel.Controls.SetChildIndex(this.personNameTxt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label4, 0);
+            this.leftPanel.Controls.SetChildIndex(this.Phone1Txt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label5, 0);
+            this.leftPanel.Controls.SetChildIndex(this.phone2Txt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label6, 0);
+            this.leftPanel.Controls.SetChildIndex(this.addressTxt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label7, 0);
+            this.leftPanel.Controls.SetChildIndex(this.ntnTxt, 0);
             // 
             // rightPanel
             // 
@@ -49,6 +87,114 @@ namespace ims
             // panel2
             // 
             this.panel2.Size = new System.Drawing.Size(1146, 40);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label2.Location = new System.Drawing.Point(4, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Supplier Name/Company";
+            // 
+            // supplierCompanyTxt
+            // 
+            this.supplierCompanyTxt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.supplierCompanyTxt.Location = new System.Drawing.Point(7, 117);
+            this.supplierCompanyTxt.Name = "supplierCompanyTxt";
+            this.supplierCompanyTxt.Size = new System.Drawing.Size(237, 23);
+            this.supplierCompanyTxt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label3.Location = new System.Drawing.Point(4, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Contact Person";
+            // 
+            // personNameTxt
+            // 
+            this.personNameTxt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personNameTxt.Location = new System.Drawing.Point(7, 163);
+            this.personNameTxt.Name = "personNameTxt";
+            this.personNameTxt.Size = new System.Drawing.Size(237, 23);
+            this.personNameTxt.TabIndex = 5;
+            // 
+            // Phone1Txt
+            // 
+            this.Phone1Txt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Phone1Txt.Location = new System.Drawing.Point(7, 212);
+            this.Phone1Txt.Name = "Phone1Txt";
+            this.Phone1Txt.Size = new System.Drawing.Size(237, 23);
+            this.Phone1Txt.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label4.Location = new System.Drawing.Point(4, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Phone 1:";
+            // 
+            // phone2Txt
+            // 
+            this.phone2Txt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phone2Txt.Location = new System.Drawing.Point(7, 259);
+            this.phone2Txt.Name = "phone2Txt";
+            this.phone2Txt.Size = new System.Drawing.Size(237, 23);
+            this.phone2Txt.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label5.Location = new System.Drawing.Point(4, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Phone 2:";
+            // 
+            // addressTxt
+            // 
+            this.addressTxt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressTxt.Location = new System.Drawing.Point(7, 307);
+            this.addressTxt.Name = "addressTxt";
+            this.addressTxt.Size = new System.Drawing.Size(237, 23);
+            this.addressTxt.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label6.Location = new System.Drawing.Point(4, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Address";
+            // 
+            // ntnTxt
+            // 
+            this.ntnTxt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ntnTxt.Location = new System.Drawing.Point(8, 353);
+            this.ntnTxt.Name = "ntnTxt";
+            this.ntnTxt.Size = new System.Drawing.Size(237, 23);
+            this.ntnTxt.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label7.Location = new System.Drawing.Point(5, 335);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "NTN #";
             // 
             // Supplier
             // 
@@ -59,6 +205,7 @@ namespace ims
             this.Text = "Supplier";
             this.Load += new System.EventHandler(this.Supplier_Load);
             this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
             this.rightPanel.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -66,5 +213,18 @@ namespace ims
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox personNameTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox supplierCompanyTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ntnTxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox addressTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox phone2Txt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Phone1Txt;
+        private System.Windows.Forms.Label label4;
     }
 }
