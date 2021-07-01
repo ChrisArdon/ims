e32eecad9efb4a6c5aced24ca7beb3f1e9af81b5
Chris Ardon <rockrisbl@gmail.com>
2021-06-30 18:49:26 -0600

    PurchaseInvoice modification

--- ims/PurchaseInvoice.Designer.cs
+++ ims/PurchaseInvoice.Designer.cs
@@ -29,6 +29,7 @@ namespace ims
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
             this.leftPanel.SuspendLayout();
             this.rightPanel.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -36,19 +37,34 @@ namespace ims
             // 
             // searchTxt
             // 
-            this.searchTxt.Size = new System.Drawing.Size(179, 23);
+            this.searchTxt.Location = new System.Drawing.Point(3, 21);
+            this.searchTxt.Size = new System.Drawing.Size(179, 25);
             // 
             // leftPanel
             // 
+            this.leftPanel.Controls.Add(this.label2);
             this.leftPanel.Size = new System.Drawing.Size(250, 813);
+            this.leftPanel.Controls.SetChildIndex(this.panel1, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label2, 0);
             // 
             // rightPanel
             // 
+            this.rightPanel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rightPanel.Size = new System.Drawing.Size(1141, 813);
             // 
             // panel2
             // 
             this.panel2.Size = new System.Drawing.Size(1141, 40);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Supplier";
             // 
             // PurchaseInvoice
             // 
@@ -59,6 +75,7 @@ namespace ims
             this.Text = "PurchaseInvoice";
             this.Load += new System.EventHandler(this.PurchaseInvoice_Load);
             this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
             this.rightPanel.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -66,5 +83,7 @@ namespace ims
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
     }
 }
