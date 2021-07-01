7fbdfd4abd7750100fac68de9e5d132de4760565
Chris Ardon <rockrisbl@gmail.com>
2021-06-30 18:50:40 -0600

    Supplier form added

---
+++ ims/Supplier.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace ims
+{
+    partial class Supplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftPanel.SuspendLayout();
+            this.rightPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchTxt
+            // 
+            this.searchTxt.Size = new System.Drawing.Size(184, 23);
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.Size = new System.Drawing.Size(250, 780);
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.Size = new System.Drawing.Size(1146, 780);
+            // 
+            // panel2
+            // 
+            this.panel2.Size = new System.Drawing.Size(1146, 40);
+            // 
+            // Supplier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1396, 780);
+            this.Name = "Supplier";
+            this.Text = "Supplier";
+            this.Load += new System.EventHandler(this.Supplier_Load);
+            this.leftPanel.ResumeLayout(false);
+            this.rightPanel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
