e6cee825214e136aaac2e56875f7134433fedadb
Chris Ardon <rockrisbl@gmail.com>
2021-07-05 19:44:49 -0600

    updateSupplier added to updation

--- ims/updation.cs
+++ ims/updation.cs
@@ -94,5 +94,50 @@ namespace ims
                 MainClass.ShowMSG(ex.Message, "Error...", "Error");
             }
         }
+        public void updateSupplier(int supID, string company, string person, string phone1, string address, Int16 status, string phone2 = null, string ntn = null)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("st_updateSupplier", MainClass.con); //Here we are using the stored procedure
+                cmd.CommandType = CommandType.StoredProcedure; //We specify the type of command that is a stored procedure
+                                                               //We proceed to fill the parameters
+                cmd.Parameters.AddWithValue("@company", company);
+                cmd.Parameters.AddWithValue("@conPerson", person);
+                cmd.Parameters.AddWithValue("@phone1", phone1);
+                //phone 2 could get a null result, for that we make an if to prevente any error
+                if (phone2 == null)
+                {
+                    cmd.Parameters.AddWithValue("@phone2", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@phone2", phone2);
+
+                }
+                cmd.Parameters.AddWithValue("@address", address);
+                if (ntn == null)
+                {
+                    cmd.Parameters.AddWithValue("@ntn", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@ntn", ntn);
+
+                }
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.Parameters.AddWithValue("@suppID", supID);
+
+                MainClass.con.Open();
+                cmd.ExecuteNonQuery();
+                MainClass.con.Close();
+                MainClass.ShowMSG(company + " updated to the system successfully", "Success...", "Success");
+            }
+            catch (Exception ex)
+            {
+
+                MainClass.con.Close();
+                MainClass.ShowMSG(ex.Message, "Error...", "Error");
+            }
+        }
     }
 }
