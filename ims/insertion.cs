81103189aedd11d746a2570c64e896a5cd416019
Chris Ardon <rockrisbl@gmail.com>
2021-07-05 19:39:06 -0600

    insertSupplier added to Insertion

--- ims/insertion.cs
+++ ims/insertion.cs
@@ -91,31 +91,42 @@ namespace ims
                 MainClass.ShowMSG(ex.Message, "Error...", "Error");
             }
         }
-        public void insertSupplier(string company, string person, float price, int catID, DateTime? expiry = null)
+        public void insertSupplier(string company, string person, string phone1, string address, Int16 status, string phone2=null, string ntn=null)
         {
             try
             {
-                SqlCommand cmd = new SqlCommand("st_productInsert", MainClass.con); //Here we are using the stored procedure
+                SqlCommand cmd = new SqlCommand("st_insertSupplier", MainClass.con); //Here we are using the stored procedure
                 cmd.CommandType = CommandType.StoredProcedure; //We specify the type of command that is a stored procedure
                                                                //We proceed to fill the parameters
-                cmd.Parameters.AddWithValue("@name", product);
-                cmd.Parameters.AddWithValue("@barcode", barcode);
-                cmd.Parameters.AddWithValue("@price", price);
-                //We specify if the expiry date is null or if it has a value
-                if (expiry == null)
+                cmd.Parameters.AddWithValue("@company", company);
+                cmd.Parameters.AddWithValue("@conPerson", person);
+                cmd.Parameters.AddWithValue("@phone1", phone1);
+                //phone 2 could get a null result, for that we make an if to prevente any error
+                if (phone2 == null)
                 {
-                    cmd.Parameters.AddWithValue("@expiry", DBNull.Value);
+                    cmd.Parameters.AddWithValue("@phone2", DBNull.Value);
                 }
                 else
                 {
-                    cmd.Parameters.AddWithValue("@expiry", expiry);
-                }
-                cmd.Parameters.AddWithValue("@catID", catID);
+                    cmd.Parameters.AddWithValue("@phone2", phone2);
 
+                }
+                cmd.Parameters.AddWithValue("@address", address);
+                if(ntn == null)
+                {
+                    cmd.Parameters.AddWithValue("@ntn", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@ntn", ntn);
+
+                }
+                cmd.Parameters.AddWithValue("@status", status);
+                
                 MainClass.con.Open();
                 cmd.ExecuteNonQuery();
                 MainClass.con.Close();
-                MainClass.ShowMSG(product + " added to the system successfully", "Success...", "Success");
+                MainClass.ShowMSG(company + " added to the system successfully", "Success...", "Success");
             }
             catch (Exception ex)
             {
