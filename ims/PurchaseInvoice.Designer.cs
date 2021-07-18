49d912445528117c1a808131370988e9a849b8ac
Chris Ardon <rockrisbl@gmail.com>
2021-07-18 04:21:27 -0600

    barcodetxt functionability changed

--- ims/PurchaseInvoice.Designer.cs
+++ ims/PurchaseInvoice.Designer.cs
@@ -40,6 +40,8 @@ namespace ims
             this.pupTxt = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.cartBtn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.leftPanel.SuspendLayout();
             this.rightPanel.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -52,6 +54,8 @@ namespace ims
             // 
             // leftPanel
             // 
+            this.leftPanel.Controls.Add(this.label8);
+            this.leftPanel.Controls.Add(this.label7);
             this.leftPanel.Controls.Add(this.cartBtn);
             this.leftPanel.Controls.Add(this.pupTxt);
             this.leftPanel.Controls.Add(this.label6);
@@ -76,6 +80,8 @@ namespace ims
             this.leftPanel.Controls.SetChildIndex(this.label6, 0);
             this.leftPanel.Controls.SetChildIndex(this.pupTxt, 0);
             this.leftPanel.Controls.SetChildIndex(this.cartBtn, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label7, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label8, 0);
             // 
             // panel1
             // 
@@ -195,6 +201,26 @@ namespace ims
             this.cartBtn.Text = "ADD TO CART";
             this.cartBtn.UseVisualStyleBackColor = true;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(5, 363);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Total Amount";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.label8.Location = new System.Drawing.Point(71, 378);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 46);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "0.00";
+            // 
             // PurchaseInvoice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -224,5 +250,7 @@ namespace ims
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox productTxt;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
     }
 }
