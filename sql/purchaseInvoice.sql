99bee2aa8243505fbd2fcf59f1b419865d515379
Chris Ardon <rockrisbl@gmail.com>
2021-07-18 04:26:03 -0600

    st_getProductbyBarcoded modifided: Added barcode parameter

--- sql/purchaseInvoice.sql
+++ sql/purchaseInvoice.sql
@@ -4,5 +4,6 @@ as
 select
 p.pro_id as 'Product ID',
 p.pro_name as 'Product',
-p.pro_price as 'Price'
+p.pro_price as 'Price',
+p.pro_barcode as 'Barcode'
 from products p where p.pro_barcode = @barcode
