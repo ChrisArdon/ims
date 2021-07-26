4a5dd6e7f024811d224b9195790b31327d72778a
Chris Ardon <rockrisbl@gmail.com>
2021-07-25 21:39:47 -0600

    purchaseInvoice.sql modified

--- sql/purchaseInvoice.sql
+++ sql/purchaseInvoice.sql
@@ -36,12 +36,26 @@ create procedure st_insertPurchaseInvoiceDetails
 as
 insert into purchaseInvoiceDetails values (@purchaseID, @proID, @quan, @totPrice)
 
-create procedure st_getPurchaseInvoiceList
+alter procedure st_getPurchaseInvoiceList
 @month int,
 @year int
 as
 select
 pii.pi_id as 'ID',
-su.sup_address as 'Company'
+su.sup_company+' '+FORMAT(pii.pi_date,'dd-MMM-yyyy') as 'Company'
 from purchaseInvoice pii inner join supplier su on su.sup_id = pii.pi_suppID
 where month(pii.pi_date) = @month and year(pii.pi_date) = @year
+
+alter procedure st_getPurchaseInvoiceDetails
+@pid bigint
+as
+select 
+pid.pid_proID as 'Product ID',
+p.pro_name as 'Product',
+pid.pid_proquan as 'Quantity',
+pid.pid_totprice as 'Total Price',
+p.pro_price as 'Per Unit Price'
+from purchaseInvoice pii 
+inner join purchaseInvoiceDetails pid 
+inner join products p on p.pro_id = pid.pid_proID on pii.pi_id = pid.pid_purchaseID
+where pii.pi_id = @pid
